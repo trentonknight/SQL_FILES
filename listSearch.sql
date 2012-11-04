216b942b3d7c523ac3b0e6efff1ebe45e90a195d
trentonknight <trentonknight@gmail.com>
2012-11-04 16:23:12 -0500

    branches book sums.

--- listSearch.sql
+++ listSearch.sql
@@ -2,4 +2,7 @@
 Select Title FROM TITLE WHERE Publisher='Sams House';
 /*find all the branches associated with author Anna Elliot*/
 SELECT HOLDING.B_id FROM HOLDING LEFT JOIN TITLE ON HOLDING.T_id=TITLE.T_id WHERE TITLE.Author='Anna Elliott' ORDER BY TITLE.Author;
-
+/*Get sum of total books from each branch*/
+SELECT SUM(COPIES) AS CopiesB1 FROM HOLDING WHERE HOLDING.B_id='B1';
+SELECT SUM(COPIES) AS CopiesB2 FROM HOLDING WHERE HOLDING.B_id='B2';
+SELECT SUM(COPIES) AS CopiesB3 FROM HOLDING WHERE HOLDING.B_id='B3';
