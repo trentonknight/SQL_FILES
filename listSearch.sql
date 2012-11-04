59056af001d6d5c973dbbdd9dd968e5e607b6da4
trentonknight <trentonknight@gmail.com>
2012-11-04 16:14:38 -0500

    added author search.

--- listSearch.sql
+++ listSearch.sql
@@ -1,3 +1,5 @@
 /*find all books published by Sams House*/
 Select Title FROM TITLE WHERE Publisher='Sams House';
+/*find all the branches associated with author Anna Elliot*/
+SELECT HOLDING.B_id FROM HOLDING LEFT JOIN TITLE ON HOLDING.T_id=TITLE.T_id WHERE TITLE.Author='Anna Elliott' ORDER BY TITLE.Author;
 
